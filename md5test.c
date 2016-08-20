ecf90ad5bfa162a42b961908e468f492ba8874aa
myl <muyunlei@gmail.com>
2016-08-20 23:14:18 +0800

    add md5

--- md5test.c
+++ md5test.c
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <openssl/md5.h>
 
 int main()
@@ -10,8 +11,14 @@ int main()
 	char	*mac	= {"muyunlei"};
 	char	*md;
 	md = (char *)MD5((const unsigned char *)mac, sizeof(mac), NULL);
-	int len = strlen(md);
-	printf("%d,%x\n", len, md);
+	int	len = strlen((char *)md);
+	char	strmd5[1024] = {0};
+	int	i;
+	for(i=0;i<len;++i)
+	{
+		sprintf(strmd5+strlen(strmd5),"%02X",md[i]&0xFF);
+	}
+	printf("%d,%s\n", len, strmd5);
 	
 	return 0;
 }
